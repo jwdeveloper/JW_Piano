7c5120dbac1ea912955065110992d584f7bc1afd
Jacek Wolniewicz <jacekwoln@gmail.com>
2022-08-03 20:01:03 +0200

    update texturepack

---
+++ src/main/java/jw/piano/listeners/ClearArmorstandsListerner.java
@@ -0,0 +1,40 @@
+package jw.piano.listeners;
+
+import jw.piano.service.PianoDataService;
+import jw.piano.service.PianoService;
+import jw.spigot_fluent_api.desing_patterns.dependecy_injection.annotations.Inject;
+import jw.spigot_fluent_api.desing_patterns.dependecy_injection.annotations.Injection;
+import jw.spigot_fluent_api.fluent_events.EventBase;
+import jw.spigot_fluent_api.fluent_logger.FluentLogger;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.world.ChunkLoadEvent;
+
+@Injection(lazyLoad = false)
+public class ClearArmorstandsListerner extends EventBase
+{
+
+    private PianoDataService dataService;
+
+    @Inject
+    public ClearArmorstandsListerner(PianoDataService service)
+    {
+        this.dataService = service;
+        FluentLogger.log("Siema 2222");
+    }
+
+    @EventHandler
+    public void onChunkLoad(ChunkLoadEvent chunkLoadEvent)
+    {
+
+        for(var a : dataService.get())
+        {
+            if(a.getLocation().getChunk().equals(chunkLoadEvent.getChunk()))
+            {
+                var e = chunkLoadEvent.getChunk().getEntities();
+                FluentLogger.log("Siema "+e.length);
+            }
+        }
+    }
+
+
+}
