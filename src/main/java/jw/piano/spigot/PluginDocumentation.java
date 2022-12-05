e9a695d352f5ffa433efec681e7ca03d6962e5f5
JW <jacekwoln@gmail.com>
2022-12-05 01:15:32 +0100

    Update

--- src/main/java/jw/piano/spigot/PluginDocumentation.java
+++ src/main/java/jw/piano/spigot/PluginDocumentation.java
@@ -14,13 +14,13 @@ public class PluginDocumentation extends DocumentationDecorator {
 
         addVideo("https://www.youtube.com/watch?v=F4iKXAMIioo&feature=emb_logo&ab_channel=JW",documentation);
         addVideo("https://www.youtube.com/watch?v=AxljLMjh4Ac&feature=emb_logo&ab_channel=JW",documentation);
-        addImage("https://proxy.spigotmc.org/65d5465e8dd82d7e765c4b783222f26fc31eb1e1?url=https%3A%2F%2Fwww.linkpicture.com%2Fq%2F8_375.png",documentation);
-        addImage("https://proxy.spigotmc.org/203a8caf5aced0a92081d4df8329e46a6630deab?url=https%3A%2F%2Fwww.linkpicture.com%2Fq%2FSupport-13.png",documentation);
+        addImage("https://raw.githubusercontent.com/jwdeveloper/JW_Piano/master/resources/images/style.png",documentation);
+        addImage("https://raw.githubusercontent.com/jwdeveloper/JW_Piano/master/resources/images/webclient.png",documentation);
     }
 
 
     private void createHeader(Documentation documentation) {
-        addImage("https://proxy.spigotmc.org/9ca3b3577528bb7fc9bbfd2f11cec73c5c2f2109?url=https%3A%2F%2Fwww.linkpicture.com%2Fq%2Fpiano.png",documentation);
+        addImage("https://raw.githubusercontent.com/jwdeveloper/JW_Piano/master/resources/images/banner.png",documentation);
         addText("<p align=\"center\">",documentation,"spigot-ignore", "plugin-ignore");
         addImageWithLink("https://raw.githubusercontent.com/jwdeveloper/SpigotFluentAPI/master/resources/social-media/discord.png", PluginConfig.DISCORD_URL, documentation);
         addImageWithLink( "https://raw.githubusercontent.com/jwdeveloper/SpigotFluentAPI/master/resources/social-media/github.png", PluginConfig.GITHUB_URL, documentation);
